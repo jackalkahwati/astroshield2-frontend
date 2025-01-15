4e2cd8c0287874bb25c9659f1b7a1779e79eb678
Jack Al-Kahwati <Jack@thestardrive.com>
2025-01-14 18:36:13 -0800

    fix: Update API URL to match new deployment

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   swcMinify: true,
   poweredByHeader: false,
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL
+    NEXT_PUBLIC_API_URL: 'https://web-production-9dd7.up.railway.app'
   }
 }
 
